1c5e4b6a44f644bd2d96d53ca68687af692bd5f6
VaishnaviNairD <41292233+VaishnaviNairD@users.noreply.github.com>
2018-07-19 14:55:40 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,18 @@
+
+
+public class Positive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=8;
+if(num>0)
+{
+	System.out.println("Positive");
+}
+else
+{
+	System.out.println("Zero");
+}
+	}
+
+}
