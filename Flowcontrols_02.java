d376aa29fd68591cd575b15c9366e3d931b96c1a
VaishnaviNairD <41292233+VaishnaviNairD@users.noreply.github.com>
2018-07-24 18:36:46 +0530

    Create Flowcontrols_02.java

---
+++ Flowcontrols_02.java
@@ -0,0 +1,19 @@
+public class Flowcontrols_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+Scanner s = new Scanner(System.in);
+System.out.println("enter the number");
+i  = s.nextInt();
+if( i%2==0)
+{
+	System.out.println("the given number is even");
+}
+else
+{ 
+	System.out.println("the given number is odd");
+}
+	}
+
+}
