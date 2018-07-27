aeb3253d4c55f56b8dd5bbaa6f2e0ab1fb2a6da7
VaishnaviNairD <41292233+VaishnaviNairD@users.noreply.github.com>
2018-07-27 12:12:24 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,10 @@
+public class Reverse {
+	    public static void main(String[] args)
+	    {
+	        String input = "aabbcc";
+	        StringBuilder input1 = new StringBuilder();
+	        input1.append(input);
+	        input1 = input1.reverse();
+	        System.out.println(input1);
+}
+}
