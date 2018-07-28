7526e88c662b520e9959584f04e75725f5e709ae
VaishnaviNairD <41292233+VaishnaviNairD@users.noreply.github.com>
2018-07-28 15:03:48 +0530

    Create AppendAPeriod.java

---
+++ AppendAPeriod.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AppendAPeriod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the String");
+String s=sc.nextLine();
+String c=s.concat(".");
+System.out.println(c);
+
+
+	}
+
+}
