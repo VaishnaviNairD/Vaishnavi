90632f5e1b93e6922454e9e59b13e2239bdfc82a
VaishnaviNairD <41292233+VaishnaviNairD@users.noreply.github.com>
2018-07-28 16:15:24 +0530

    Create Roman.java

---
+++ Roman.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Roman {
+public static void main(String[] args) {
+		// TODO Auto-generated method st
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the String");
+		String l=s.nextLine();
+if(l=="VI")
+{
+	System.out.print("6");
+}
+else if(l=="X")
+{
+	System.out.print("10");
+}
+else
+{
+	System.out.print("11");
+}
+	}
+}
